64bc82f8f0a65d73a3385b22d425e17473b469e7
Tanagorn-Jitjumsee <65001308107@udru.ac.th>
2024-10-09 13:56:09 +0700

    Update Image-segmentation.js

--- Image-segmentation.js
+++ Image-segmentation.js
@@ -15,8 +15,8 @@
 import { ImageSegmenter, SegmentationMask, FilesetResolver } from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.2";
 
 // Get DOM elements
-const video = document.getElementById("webcam") as HTMLVideoElement;
-const canvasElement = document.getElementById("canvas") as HTMLCanvasElement;
+const video = document.getElementById("webcam") ;
+const canvasElement = document.getElementById("canvas")t;
 const canvasCtx = canvasElement.getContext("2d");
 const webcamPredictions = document.getElementById("webcamPredictions");
 const demosSection: HTMLElement = document.getElementById("demos");
